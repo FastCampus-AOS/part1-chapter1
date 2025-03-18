c69009c817f54022ccbb98d57585c3ab423ed3e0
ParkHyunSu99 <phs0767@pineone.com>
2025-03-19 00:05:32 +0900

    숫자세기 기능 구현

--- app/src/main/java/fastcampus/aos/part1/part1_chapter1/MainActivity.kt
+++ app/src/main/java/fastcampus/aos/part1/part1_chapter1/MainActivity.kt
@@ -18,12 +18,21 @@ class MainActivity : AppCompatActivity() {
         val resetButton = findViewById<Button>(R.id.resetButton)
         val plusButton = findViewById<Button>(R.id.plusButton)
 
+        var number = 0
+
+
         resetButton.setOnClickListener {
+            number = 0
+            numberTextView.text = number.toString()
             Log.d("onClick", "리셋 버튼이 클릭 됐습니다")
+            Log.d("onClick", "리셋된 숫자는 $number")
         }
 
         plusButton.setOnClickListener {
+            number += 1
+            numberTextView.text = number.toString()
             Log.d("onClick", "플러스 버튼이 클릭 됐습니다")
+            Log.d("onClick", "플러스 된 숫자는 $number")
         }
     }
 }
